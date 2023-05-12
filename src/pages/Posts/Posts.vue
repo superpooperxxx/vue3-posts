452a89817d9234bc99ac542205a9d3e4e22b4017
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-05-12 13:39:25 +0300

    added filtering by user

--- src/pages/Posts/Posts.vue
+++ src/pages/Posts/Posts.vue
@@ -12,13 +12,30 @@ export default {
       page: 1,
       limit: 5,
       limitOptions: [5, 10, 15, 20],
-      usersId: [],
-      totalPosts: 100,
+      usersIds: [],
+      userFilter: "",
+      totalPosts: 0,
     };
+  },
+  mounted() {
+    this.getUsersIds();
   },
   methods: {
     setTotalPosts(total) {
       this.totalPosts = total;
+    },
+    async getUsersIds() {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        const ids = res.data.map((user) => user.id);
+
+        this.usersIds = ids;
+      } catch (error) {
+      } finally {
+      }
     },
   },
 };
@@ -32,10 +49,23 @@ export default {
         v-model="limit"
         :options="limitOptions"
       />
+
+      <Select
+        :title="'Filter by user'"
+        v-model="userFilter"
+        :options="usersIds"
+      >
+        <option value="">All</option>
+      </Select>
     </div>
 
     <div class="posts-page__posts-list">
-      <PostsList :limit="limit" :page="page" @setTotalPosts="setTotalPosts" />
+      <PostsList
+        :limit="limit"
+        :page="page"
+        @setTotalPosts="setTotalPosts"
+        :userFilter="userFilter"
+      />
     </div>
 
     <div class="posts-page__btns">
